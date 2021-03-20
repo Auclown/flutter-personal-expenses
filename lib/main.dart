34c7d2072cbbfbcc789764f4b0e20ff1f84fe4aa
Auclown <auclown@gmail.com>
2021-03-20 23:15:04 +1300

    20th/Mar/2021 - String interpolation implemented

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class MyHomePage extends StatelessWidget {
                     ),
                     padding: EdgeInsets.all(12),
                     child: Text(
-                      tx.amount.toString(),
+                      "\$${tx.amount}",
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
