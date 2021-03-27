f1325afd186fcb0ff564165a87bd17bf7dd30bbf
Auclown <auclown@gmail.com>
2021-03-27 23:29:37 +1300

    27th/Mar/2021 - DatePicker added

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
+                button: TextStyle(color: Colors.white),
               ),
         ),
       ),
